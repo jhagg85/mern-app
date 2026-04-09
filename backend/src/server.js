1d5770787dec0389217cfb85fbfddc78db8384df
jhagg85 <jhaggy85@gmail.com>
2026-04-10 07:49:09 +0800

    - Added @upstash/ratelimit and @upstash/redis dependencies in package.json and package-lock.json. - Implemented MongoDB connection logic in a new db.js file. - Introduced rate limiting middleware in rateLimiter.js and configured it in server.js. - Created upstash.js for rate limit configuration.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from '../config/db.js';
+import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -12,13 +13,22 @@ if (!PORT) {
   throw new Error('Missing PORT environment variable');
 }
 
-connectDB();
-
 // Middleware
-app.use(express.json());
+app.use(express.json()); // parse the incoming request body in JSON format
+
+// Custom middleware
+// app.use((req, res, next) => {
+//   // console.log('we just got a request!');
+//   console.log(`req method is ${req.method} and the url is ${req.url}`);
+//   next();
+// });
+
+app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
